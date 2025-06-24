469d4ca33f4009c654b0dc32d51088670aa55213
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-06-24 16:34:14 +0200

    use one config map for hc scripts, add custom scripts config map

--- soperator/modules/slurm/main.tf
+++ soperator/modules/slurm/main.tf
@@ -183,9 +183,6 @@ resource "helm_release" "soperator_fluxcd_cm" {
 
       nfs = var.nfs
 
-      default_prolog_enabled = var.default_prolog_enabled
-      default_epilog_enabled = var.default_epilog_enabled
-
       nccl_topology_type = var.nccl_topology_type
       nccl_benchmark = {
         enable         = var.nccl_benchmark_enable
